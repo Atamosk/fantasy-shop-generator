4cc7e0160eb502959e147b98180d7e02990c741b
William <williamedwardkenney@gmail.com>
2025-07-02 23:52:16 +0000

    Commit before starting to use alasql
    Co-authored-by: mgaydash <mgaydash@users.noreply.github.com>

--- src/app/app.js
+++ src/app/app.js
@@ -1,2 +1,14 @@
-let hello = "Hello, World!"
-console.log(hello);
+const alasql = require('alasql');
+const itemList = require("../data/items.json");
+
+let returnArray = [];
+itemList.forEach(element => {
+    if (element.type == "Combat")
+    {
+        returnArray.push(element.name);
+    }
+});
+
+// SELECT item WHERE type = 'Combat';
+
+console.log(returnArray);
