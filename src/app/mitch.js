612bdf271495e1f49401c25aebdda3c2090fbdc1
mgaydash <mitchell@gaydash.net>
2025-09-04 00:17:51 +0000

    Add spells queries

--- src/app/mitch.js
+++ src/app/mitch.js
@@ -2,6 +2,7 @@ const alasql = require('alasql');
 const itemList = require("../data/items.json");
 
 const dnd_items = require('dnd-data').items;
+const dnd_spells = require('dnd-data').spells;
 
 console.log("Unique Types")
 console.log(alasql("SELECT DISTINCT type FROM ?", [itemList]));
@@ -20,3 +21,18 @@ console.log(alasql("SELECT *  FROM ? LIMIT 1", [dnd_items]));
 
 console.log("Distinct dnd-items Item Types")
 console.log(alasql("SELECT DISTINCT properties->('Item Type') FROM ?", [dnd_items]));
+
+console.log("Distinct dnd-items Item Types")
+console.log(alasql("SELECT DISTINCT properties->('Item Type') FROM ?", [dnd_items]));
+
+console.log("dnd-items Potions")
+console.log(alasql("SELECT * FROM ? WHERE properties->('Item Type') = 'Potion'", [dnd_items]));
+
+console.log("1 Spell")
+console.log(alasql("SELECT * FROM ? LIMIT 1", [dnd_spells]));
+
+console.log("All Schools")
+console.log(alasql("SELECT DISTINCT properties->School FROM ?", [dnd_spells]));
+
+console.log("Conjuration Spells")
+console.log(alasql("SELECT * FROM ? WHERE LOWER(properties->School) LIKE '%conjuration%'", [dnd_spells]));
